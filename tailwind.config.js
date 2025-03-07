e7c863a465a6225d04ed82f0a19b1f67ead3e742
Andrew Niemantsverdriet <andrew@flowforms.io>
2025-03-07 11:00:53 -0700

    updated to Laravel 12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'selector',
-
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        "./vendor/livewire/flux-pro/stubs/**/*.blade.php",
-        "./vendor/livewire/flux/stubs/**/*.blade.php",
+        './resources/views/**/*.blade.php',
     ],
+
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', 'Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
-    plugins: [],
+
+    plugins: [forms, typography],
 };
