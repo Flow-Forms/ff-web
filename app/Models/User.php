3808c3271d223160ff372112a164e513e2e728e0
Andrew Niemantsverdriet <andrew@flowforms.io>
2025-11-04 09:56:31 -0700

    Removes profile photo functionality
    Removes the profile photo functionality from the User model by removing the `HasProfilePhoto` trait and the `profile_photo_url` attribute.
    
    This change aims to simplify the user model and remove unused features.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -16,7 +14,6 @@ class User extends Authenticatable
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
-    use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
 
@@ -48,9 +45,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $appends = [
-        'profile_photo_url',
-    ];
+    protected $appends = [];
 
     /**
      * Get the attributes that should be cast.
